67dc4c460fca8f5ef848864f6a22c7c072b15ab8
xanzitech-collab <xanzitech@gmail.com>
2026-08-05 23:33:58 +0200

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Explicitly use src directory
+  dir: "./src",
 };
 
 export default nextConfig;
